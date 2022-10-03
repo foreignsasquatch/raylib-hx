9ebfaea97e96b6664b4d6cf6b0b02ccc56d32685
Ratul Krisna <ratul.krisna01@protonmail.com>
2022-10-03 11:06:35 +0530

    Fixes #25

--- examples/BasicWindow.hx
+++ examples/BasicWindow.hx
@@ -1,17 +1,17 @@
-import raylib.Rl.*;
+import Rl.*;
 
 class BasicWindow {
   static function main() {
     initWindow(800, 450, "hxRaylib [core] example - basic window");
     setTargetFPS(60);
 
-    var vec:raylib.Vector2 = new raylib.Vector2(100, 100);
+    var vec:Rl.Vector2 = Rl.Vector2.create(0, 0);
     var t = loadTexture("parrot.png");
 
     while(!windowShouldClose()) {
       beginDrawing();
-      clearBackground(raylib.Rl.Colors.WHITE);
-      drawTextureV(t, vec, raylib.Rl.Colors.WHITE);
+      clearBackground(Rl.Colors.WHITE);
+      drawTextureV(t, vec, Rl.Colors.WHITE);
       endDrawing();
     }
 
