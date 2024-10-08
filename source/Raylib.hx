8940add44890112bc954a59bf39cd3f5dafa8d56
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-08 22:47:06 +0300

    Update Raylib.hx

--- source/Raylib.hx
+++ source/Raylib.hx
@@ -39,20 +39,19 @@ extern class RayVector2
 	@:native('Vector2')
 	static function alloc():RayVector2;
 
-	var x:Single;
-	var y:Single;
-}
-
-extern class Vector2 extends cpp.Struct<RayVector2>
-{
-	static inline function create(x:Single, y:Single):Vector2
+	static inline function create(x:Single, y:Single):RayVector2
 	{
 		final vec2:RayVector2 = RayVector2.alloc();
 		vec2.x = x;
 		vec2.y = y;
-		return cast vec2;
+		return vec2;
 	}
+
+	var x:Single;
+	var y:Single;
 }
+
+typedef Vector2 = cpp.Struct<RayVector2>;
 
 @:buildXml('<include name="${haxelib:raylib-hx}/project/Build.xml" />')
 @:include('raylib.h')
