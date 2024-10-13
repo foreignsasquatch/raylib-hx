17aa21ae4e3fe6244d9c0a47bc14260bc74f73d6
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-13 18:08:37 +0300

    Create FloatPointer.hx

---
+++ source/utils/FloatPointer.hx
@@ -0,0 +1,29 @@
+package utils;
+
+import haxe.extern.AsVar;
+
+abstract FloatPointer(cpp.RawPointer<Single>) from cpp.RawPointer<Single> to cpp.RawPointer<Single>
+{
+	inline function new(ptr:cpp.RawPointer<Single>):Void
+	{
+		this = ptr;
+	}
+
+	@:from
+	public static inline function fromValue(value:Single):FloatPointer
+	{
+		return new FloatPointer(cpp.RawPointer.addressOf((value : AsVar<Single>));
+	}
+
+	@:from
+	public static inline function fromArray(value:Array<Single>):FloatPointer
+	{
+		return new FloatPointer(cpp.Pointer.ofArray((value : AsVar<Array<Single>>)).raw);
+	}
+
+	@:to
+	public inline function toSingle():Single
+	{
+		return this[0];
+	}
+}
