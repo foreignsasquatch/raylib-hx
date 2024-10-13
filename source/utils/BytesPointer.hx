7ab246219b8340e4a7b6c93881ab0ac2fb17890f
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-13 19:10:28 +0300

    Create BytesPointer.hx

---
+++ source/utils/BytesPointer.hx
@@ -0,0 +1,24 @@
+package utils;
+
+import haxe.io.Bytes;
+import haxe.io.BytesData;
+
+extern abstract BytesPointer(cpp.RawPointer<cpp.UInt8>) from cpp.RawPointer<cpp.UInt8> to cpp.RawPointer<cpp.UInt8>
+{
+	inline function new(ptr:cpp.RawPointer<cpp.UInt8>):Void
+	{
+		this = ptr;
+	}
+
+	@:from
+	public static inline function fromBytes(value:Bytes):BytesPointer
+	{
+		return fromBytesData(value.getData());
+	}
+
+	@:from
+	public static inline function fromBytesData(value:BytesData):BytesPointer
+	{
+		return new BytesPointer(cpp.Pointer.ofArray(value).raw);
+	}
+}
