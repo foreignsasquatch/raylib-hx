bd673dc955c633542188ee49218d68d2996e9d30
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-13 18:32:22 +0300

    Update FloatPointer.hx

--- source/utils/FloatPointer.hx
+++ source/utils/FloatPointer.hx
@@ -1,7 +1,5 @@
 package utils;
 
-import haxe.extern.AsVar;
-
 extern abstract FloatPointer(cpp.RawPointer<Single>) from cpp.RawPointer<Single> to cpp.RawPointer<Single>
 {
 	inline function new(ptr:cpp.RawPointer<Single>):Void
@@ -12,13 +10,13 @@ extern abstract FloatPointer(cpp.RawPointer<Single>) from cpp.RawPointer<Single>
 	@:from
 	public static inline function fromValue(value:Single):FloatPointer
 	{
-		return new FloatPointer(cpp.RawPointer.addressOf((value : AsVar<Single>)));
+		return new FloatPointer(cpp.Pointer.addressOf(value).raw);
 	}
 
 	@:from
 	public static inline function fromArray(value:Array<Single>):FloatPointer
 	{
-		return new FloatPointer(cpp.Pointer.ofArray((value : AsVar<Array<Single>>)).raw);
+		return new FloatPointer(cpp.Pointer.ofArray(value).raw);
 	}
 
 	@:to
