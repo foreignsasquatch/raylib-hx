6dafbbe331b585968e11d24b114007bff33ecfdd
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-13 18:46:38 +0300

    Create IntPointer.hx

---
+++ source/utils/IntPointer.hx
@@ -0,0 +1,27 @@
+package utils;
+
+extern abstract IntPointer(cpp.RawPointer<Int>) from cpp.RawPointer<Int> to cpp.RawPointer<Int>
+{
+	inline function new(ptr:cpp.RawPointer<Int>):Void
+	{
+		this = ptr;
+	}
+
+	@:from
+	public static inline function fromValue(value:Int):IntPointer
+	{
+		return new IntPointer(cpp.Pointer.addressOf(value).raw);
+	}
+
+	@:from
+	public static inline function fromArray(value:Array<Int>):IntPointer
+	{
+		return new IntPointer(cpp.Pointer.ofArray(value).raw);
+	}
+
+	@:to
+	public inline function toInt():Int
+	{
+		return this[0];
+	}
+}
