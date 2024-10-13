abfdfb7bca545ef192705098db9bfcd656cf76fd
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-13 18:50:32 +0300

    Create UnsignedPointer.hx

---
+++ source/utils/UnsignedPointer.hx
@@ -0,0 +1,27 @@
+package utils;
+
+extern abstract UnsignedPointer(cpp.RawPointer<cpp.UInt32>) from cpp.RawPointer<cpp.UInt32> to cpp.RawPointer<cpp.UInt32>
+{
+	inline function new(ptr:cpp.RawPointer<cpp.UInt32>):Void
+	{
+		this = ptr;
+	}
+
+	@:from
+	public static inline function fromValue(value:cpp.UInt32):cpp.UInt32Pointer
+	{
+		return new UnsignedPointer(cpp.Pointer.addressOf(value).raw);
+	}
+
+	@:from
+	public static inline function fromArray(value:Array<cpp.UInt32>):cpp.UInt32Pointer
+	{
+		return new UnsignedPointer(cpp.Pointer.ofArray(value).raw);
+	}
+
+	@:to
+	public inline function toUnsigned():cpp.UInt32
+	{
+		return this[0];
+	}
+}
