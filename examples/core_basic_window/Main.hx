e62d6e118c10324f45e9ee7c2189c1c9c4dd6854
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-13 23:35:14 +0300

    Create Main.hx

---
+++ examples/core_basic_window/Main.hx
@@ -0,0 +1,47 @@
+package;
+
+import Raylib;
+
+class Main
+{
+    //------------------------------------------------------------------------------------
+    // Program main entry point
+    //------------------------------------------------------------------------------------
+    public static function main():Void
+    {
+        // Initialization
+        //--------------------------------------------------------------------------------------
+        final screenWidth:Int = 800;
+        final screenHeight:Int = 450;
+
+        Raylib.initWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+        Raylib.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+        //--------------------------------------------------------------------------------------
+
+        // Main game loop
+        while (!Raylib.windowShouldClose()) // Detect window close button or ESC key
+        {
+            // Update
+            //----------------------------------------------------------------------------------
+            // TODO: Update your variables here
+            //----------------------------------------------------------------------------------
+
+            // Draw
+            //----------------------------------------------------------------------------------
+            Raylib.beginDrawing();
+
+            Raylib.clearBackground(Raylib.RAYWHITE);
+
+            Raylib.drawText("Congrats! You created your first window!", 190, 200, 20, Raylib.LIGHTGRAY);
+
+            Raylib.endDrawing();
+            //----------------------------------------------------------------------------------
+        }
+
+        // De-Initialization
+        //--------------------------------------------------------------------------------------
+        Raylib.closeWindow(); // Close window and OpenGL context
+        //--------------------------------------------------------------------------------------
+    }
+}
