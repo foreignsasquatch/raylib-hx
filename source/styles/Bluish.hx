5861ce24b004c14b90f20bc4cde49ba6b767a2ff
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-13 11:47:22 +0300

    Create Bluish.hx

---
+++ source/styles/Bluish.hx
@@ -0,0 +1,16 @@
+package styles;
+
+@:buildXml('<include name="${haxelib:raygui-impl.h}/project/Build.xml" />')
+@:include('bluish/style_bluish.h')
+@:unreflective
+extern class Bluish
+{
+	@:native('BLUISH_STYLE_PROPS_COUNT')
+	static var STYLE_PROPS_COUNT:Int;
+
+	@:native('BLUISH_STYLE_FONT_ATLAS_COMP_SIZE')
+	static var STYLE_FONT_ATLAS_COMP_SIZE:Int;
+
+	@:native('GuiLoadStyleBluish')
+	static function guiLoadStyle():Void;
+}
