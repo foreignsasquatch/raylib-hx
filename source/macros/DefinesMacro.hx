32ff021d41501c4885b65be2ac7d02e714c6adc7
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-16 16:21:51 +0300

    Create DefinesMacro.hx

---
+++ source/macros/DefinesMacro.hx
@@ -0,0 +1,18 @@
+package macros;
+
+#if macro
+@:nullSafety
+class DefinesMacro
+{
+    public static function run():Void
+    {
+        if (haxe.macro.Context.defined('RAYLIB_WEB_RESOURCES_PATH'))
+        {
+            final resourcesPath:Null<String> = haxe.macro.Context.definedValue('RAYLIB_WEB_RESOURCES_PATH');
+
+            if (resourcesPath != null && resourcesPath.length > 0)
+                haxe.macro.Compiler.define('RAYLIB_WEB_RESOURCES_PATH', sys.FileSystem.absolutePath(resourcesPath) + '@' + resourcesPath);
+        }
+    }
+}
+#end
