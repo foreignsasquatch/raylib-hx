ca58cb791c787deebd6c8d24ca0f462a6b87c8e9
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-17 10:54:19 +0300

    Update Raymath.hx

--- source/Raymath.hx
+++ source/Raymath.hx
@@ -70,4 +70,30 @@ extern class Raymath
     @:native('Vector2Clamp') static function vector2Clamp(v:RayVector2, min:RayVector2, max:RayVector2):RayVector2;
     @:native('Vector2ClampValue') static function vector2ClampValue(v:RayVector2, min:Single, max:Single):RayVector2;
     @:native('Vector2Equals') static function vector2Equals(p:RayVector2, q:RayVector2):Bool;
+
+    @:native('Vector3Zero') static function vector3Zero():RayVector3;
+    @:native('Vector3One') static function vector3One():RayVector3;
+    @:native('Vector3Add') static function vector3Add(v1:RayVector3, v2:RayVector3):RayVector3;
+    @:native('Vector3AddValue') static function vector3AddValue(v:RayVector3, add:Single):RayVector3;
+    @:native('Vector3Subtract') static function vector3Subtract(v1:RayVector3, v2:RayVector3):RayVector3;
+    @:native('Vector3SubtractValue') static function vector3SubtractValue(v:RayVector3, sub:Single):RayVector3;
+    @:native('Vector3Scale') static function vector3Scale(v:RayVector3, scalar:Single):RayVector3;
+    @:native('Vector3Multiply') static function vector3Multiply(v1:RayVector3, v2:RayVector3):RayVector3;
+    @:native('Vector3CrossProduct') static function vector3CrossProduct(v1:RayVector3, v2:RayVector3):RayVector3;
+    @:native('Vector3Perpendicular') static function vector3Perpendicular(v:RayVector3):RayVector3;
+    @:native('Vector3Length') static function vector3Length(v:RayVector3):Single;
+    @:native('Vector3LengthSqr') static function vector3LengthSqr(v:RayVector3):Single;
+    @:native('Vector3DotProduct') static function vector3DotProduct(v1:RayVector3, v2:RayVector3):Single;
+    @:native('Vector3Distance') static function vector3Distance(v1:RayVector3, v2:RayVector3):Single;
+    @:native('Vector3DistanceSqr') static function vector3DistanceSqr(v1:RayVector3, v2:RayVector3):Single;
+    @:native('Vector3Angle') static function vector3Angle(v1:RayVector3, v2:RayVector3):Single;
+    @:native('Vector3Negate') static function vector3Negate(v:RayVector3):RayVector3;
+    @:native('Vector3Divide') static function vector3Divide(v1:RayVector3, v2:RayVector3):RayVector3;
+    @:native('Vector3Normalize') static function vector3Normalize(v:RayVector3):RayVector3;
+    @:native('Vector3Project') static function vector3Project(v1:RayVector3, v2:RayVector3):RayVector3;
+    @:native('Vector3Reject') static function vector3Reject(v1:RayVector3, v2:RayVector3):RayVector3;
+    @:native('Vector3OrthoNormalize') static function vector3OrthoNormalize(v1:cpp.RawPointer<RayVector3>, v2:cpp.RawPointer<RayVector3>):Void;
+    @:native('Vector3Transform') static function vector3Transform(v:RayVector3, mat:Matrix):RayVector3;
+    @:native('Vector3RotateByQuaternion') static function vector3RotateByRayVector4(v:RayVector3, q:RayVector4):RayVector3;
+    @:native('Vector3RotateByAxisAngle') static function vector3RotateByAxisAngle(v:RayVector3, axis:RayVector3, angle:Single):RayVector3;
 }
