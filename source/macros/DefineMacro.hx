46d1933bfd6308f9febb7f048676664f4e1a4893
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-17 07:16:07 +0300

    Update DefineMacro.hx

--- source/macros/DefineMacro.hx
+++ source/macros/DefineMacro.hx
@@ -1,17 +1,21 @@
 package macros;
 
 #if macro
+import haxe.macro.Context;
+import haxe.macro.Compiler;
+import sys.FileSystem;
+
 @:nullSafety
 class DefineMacro
 {
     public static function run():Void
     {
-        if (haxe.macro.Context.defined('emscripten') && haxe.macro.Context.defined('RAYLIB_WEB_RESOURCES_PATH'))
+        if (Context.defined('emscripten') && Context.defined('RAYLIB_WEB_RESOURCES_PATH'))
         {
-            final resourcesPath:Null<String> = haxe.macro.Context.definedValue('RAYLIB_WEB_RESOURCES_PATH');
+            final resourcesPath:Null<String> = Context.definedValue('RAYLIB_WEB_RESOURCES_PATH');
 
             if (resourcesPath != null && resourcesPath.length > 0)
-                haxe.macro.Compiler.define('RAYLIB_WEB_RESOURCES_PATH', sys.FileSystem.absolutePath(resourcesPath) + '@' + resourcesPath);
+                Compiler.define('RAYLIB_WEB_RESOURCES_PATH', FileSystem.absolutePath(resourcesPath) + '@' + resourcesPath);
         }
     }
 }
