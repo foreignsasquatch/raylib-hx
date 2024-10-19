b7e8a356bd45b5192290380aece873e558032e08
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-19 12:50:58 +0000

    Alright

---
+++ source/utils/BoolConstPointer.hx
@@ -0,0 +1,33 @@
+package utils;
+
+extern abstract BoolConstPointer(cpp.RawConstPointer<Bool>) from cpp.RawConstPointer<Bool> to cpp.RawConstPointer<Bool>
+{
+    inline function new(ptr:cpp.RawConstPointer<Bool>):Void
+    {
+        this = ptr;
+    }
+
+    @:arrayAccess
+    inline function get(index:Int):Bool
+    {
+        return this[index];
+    }
+
+    @:arrayAccess
+    inline function set(index:Int, value:Bool):Void
+    {
+        this[index] = value;
+    }
+
+    @:from
+    static inline function fromValue(value:Bool):BoolConstPointer
+    {
+        return new BoolConstPointer(cpp.Pointer.addressOf(value).constRaw);
+    }
+
+    @:from
+    static inline function fromArray(value:Array<Bool>):BoolConstPointer
+    {
+        return new BoolConstPointer(cpp.Pointer.ofArray(value).constRaw);
+    }
+}
