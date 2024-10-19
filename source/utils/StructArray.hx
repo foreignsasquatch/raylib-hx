75ae12a51d42a5ce143beede360aa8efc0aef4d2
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-19 21:57:59 +0300

    Update StructArray.hx

--- source/utils/StructArray.hx
+++ source/utils/StructArray.hx
@@ -6,12 +6,12 @@ extern abstract StructArray<T>(Array<T>) from Array<T> to Array<T>
     @:arrayAccess
     inline function get(index:Int):T
     {
-        return cpp.NativeArray.address(this, index).at(0);
+        return cpp.Pointer.arrayElem(this, index).raw[0];
     }
 
     @:arrayAccess
     inline function set(index:Int, value:T):Void
     {
-        cpp.NativeArray.address(this, index).setAt(0, value);
+        cpp.Pointer.arrayElem(this, index).raw[0] = value;
     }
 }
