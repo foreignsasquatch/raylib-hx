84956d75bcc9328a340576f398eb0041a4a074b8
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-19 22:21:14 +0300

    Update StructArray.hx

--- source/utils/StructArray.hx
+++ source/utils/StructArray.hx
@@ -3,11 +3,11 @@ package utils;
 @:forward
 extern abstract StructArray<T>(Array<T>) from Array<T> to Array<T>
 {
-    /*@:arrayAccess
+    @:arrayAccess
     inline function get(index:Int):T
     {
-        return cpp.Pointer.arrayElem(this, index).raw[0];
-    }*/
+        return this[i];
+    }
 
     @:arrayAccess
     inline function set(index:Int, value:T):Void
