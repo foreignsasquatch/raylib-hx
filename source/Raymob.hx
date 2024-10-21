7375d523a337cf369191fe02f96c4947fa4974b6
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-21 12:43:49 +0300

    Create Raymob.hx

---
+++ source/Raymob.hx
@@ -0,0 +1,39 @@
+/**
+ *   raylib-hx - a Haxe binding for the library raylib, a simple and easy-to-use library to enjoy videogames programming
+ *
+ *   LICENSE: zlib/libpng
+ *
+ *   raylib-hx is licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+ *   BSD-like license that allows static linking with closed source software:
+ *
+ *   Copyright (c) 2021-2022 Ratul Krisna (@ForeignSasquatch)
+ *
+ *   This software is provided "as-is", without any express or implied warranty. In no event
+ *   will the authors be held liable for any damages arising from the use of this software.
+ *
+ *   Permission is granted to anyone to use this software for any purpose, including commercial
+ *   applications, and to alter it and redistribute it freely, subject to the following restrictions:
+ *
+ *     1. The origin of this software must not be misrepresented; you must not claim that you
+ *     wrote the original software. If you use this software in a product, an acknowledgment
+ *     in the product documentation would be appreciated but is not required.
+ *
+ *     2. Altered source versions must be plainly marked as such, and must not be misrepresented
+ *     as being the original software.
+ *
+ *     3. This notice may not be removed or altered from any source distribution.
+ */
+
+package;
+
+#if !cpp
+#error 'Raylib supports only C++ target platforms.'
+#end
+import Raylib;
+
+@:buildXml('<include name="${haxelib:raylib-hx}/project/Build.xml" />')
+@:include('raymob.h')
+@:unreflective
+extern class Raymob
+{
+}
