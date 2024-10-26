529a2546a39cfe1e969ef56d7a2cb03a65237e03
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-26 21:27:29 +0300

    Update TargetPlatform.hx

--- tools/interfaces/TargetPlatform.hx
+++ tools/interfaces/TargetPlatform.hx
@@ -1,12 +1,9 @@
 package interfaces;
 
-import hxp.HXML;
 import utils.Architecture;
 
 interface TargetPlatform
 {
-    public function new(hxml:HXML):Void;
-
     public function setup():Void;
     public function build(architecture:Architecture):Bool;
     public function supportedArchitectures():Array<Architecture>;
