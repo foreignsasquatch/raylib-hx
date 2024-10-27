45a4b2f3c9214ed4c18c8a72eb140afabdc58890
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-27 14:41:12 +0200

    Update AndroidPlatform.hx

--- tools/platforms/AndroidPlatform.hx
+++ tools/platforms/AndroidPlatform.hx
@@ -12,6 +12,7 @@ import utils.Architecture;
 class AndroidPlatform implements TargetPlatform
 {
 	private final hxml:HXML;
+
 	private final assetsDirectory:String;
 	private final cppDirectory:String;
 	private final javaDirectory:String;
