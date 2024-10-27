584be1243ffeff0fb69ea38e573094e48e38298a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-27 13:50:05 +0200

    Update AndroidPlatform.hx

--- tools/platforms/AndroidPlatform.hx
+++ tools/platforms/AndroidPlatform.hx
@@ -156,7 +156,7 @@ class AndroidPlatform implements TargetPlatform
 		}
 
 		if (System.hostPlatform != WINDOWS)
-			System.runCommand(hxml.cpp, 'chmod', ['755', './gradlew']);
+			System.runCommand(null, 'chmod', ['755', Path.join([hxml.cpp, 'gradlew']));
 
 		System.runCommand(hxml.cpp, System.hostPlatform != WINDOWS ? './gradlew' : 'gradlew');
 
