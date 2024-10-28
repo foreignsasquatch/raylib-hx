91fcfcd1a4de054e2d7cbf679000f97a064c92bd
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-28 08:39:41 +0200

    Create TargetPlatform.hx

---
+++ tools/platforms/TargetPlatform.hx
@@ -0,0 +1,42 @@
+package interfaces;
+
+import utils.Architecture;
+import utils.Config;
+
+@:nullSafety
+class TargetPlatform
+{
+	@:noCompletion
+	private final config:Config;
+
+	@:noCompletion
+	private final hxml:HXML;
+
+	public function new(config:Config, hxml:HXML):Void
+	{
+		this.config = config;
+		this.hxml = hxml;
+    }
+
+    public function setup():Void {}
+
+	public function build(architectures:Array<Architecture>):Bool
+	{
+		architectures = architectures.filter(function(arch:Architecture):Bool
+		{
+			return supportedArchitectures().contains(arch);
+		});
+
+		if (architectures.length == 0)
+		{
+			Log.error('No architectures to build after applying exclusions.');
+
+			return;
+        }
+    }
+
+    public function supportedArchitectures():Array<Architecture>
+    {
+        return [];
+    }
+}
