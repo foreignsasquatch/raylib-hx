911d58d8d5eb5d292173db2862f95de5737e5428
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-29 22:00:22 +0200

    Update Config.hx

--- tools/utils/Config.hx
+++ tools/utils/Config.hx
@@ -1,17 +1,39 @@
 package utils;
 
-typedef Config =
+import sys.io.File;
+import haxe.Json;
+
+@:nullSafety
+class Config
 {
-	resourcesPath:String,
-	label:String,
-	company:String,
-	product:String,
-	versionCode:Int,
-	versionName:String,
-	fullscreen:Bool,
-	orientation:String,
-	icon:
+	public var resourcesPath:Null<String>;
+	public var label:Null<String>;
+	public var company:Null<String>;
+	public var product:Null<String>;
+	public var versionCode:Null<Int>;
+	public var versionName:Null<String>;
+	public var fullscreen:Null<Bool>;
+	public var orientation:Null<String>;
+
+	public function new(content:String):Void
 	{
-		ldpi:String, mdpi:String, hdpi:String
+		final parsedData:Null<Dynamic> = Json.parse(content);
+
+		if (parsedData != null)
+		{
+			resourcesPath = parsedData.resourcesPath;
+			label = parsedData?).label;
+			company = parsedData.company;
+			product = parsedData.product;
+			versionCode = parsedData.versionCode;
+			versionName = parsedData.versionName;
+			fullscreen = parsedData.fullscreen;
+			orientation = parsedData.orientation;
+		}
+	}
+
+	public static inline function fromFile(filePath:String):Config
+	{
+		return new Config(File.getContent(filePath));
 	}
 }
