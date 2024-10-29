68f4bb100e61637a87cc6501d354f7b68b77a5e8
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-29 08:21:12 +0200

    Update Runner.hx

--- tools/Runner.hx
+++ tools/Runner.hx
@@ -1,9 +1,9 @@
 package;
 
-import haxe.io.Path;
 import haxe.Json;
 import hxp.HXML;
 import hxp.Log;
+import hxp.Script;
 import hxp.System;
 import platforms.AndroidPlatform;
 import platforms.TargetPlatform;
@@ -20,82 +20,112 @@ class Runner
 
 	public static function main():Void
 	{
-		final args:Array<String> = Sys.args();
-		final runDir:Null<String> = args.pop();
-		final command:Null<String> = args.shift();
-		final target:Null<String> = args.shift();
+		Script.main();
 
-		var platform:Null<TargetPlatform> = null;
+		// Create an instance of Script to parse arguments.
+		final script:Script = new Script();
 
-		if (runDir != null && command != null && target != null)
-		{
-			Sys.setCwd(runDir);
+		// Retrieve and trace various properties from the Script instance.
+		var args:Array<String> = script.args;
+		trace('args: ' + args);
 
-			switch (command)
+		var command:String = script.command;
+		trace('command: ' + command);
+
+		var commandArgs:Array<String> = script.commandArgs;
+		trace('commandArgs: ' + commandArgs);
+
+		var defines:Map<String, String> = script.defines;
+		trace('defines: ' + defines);
+
+		var flags:Map<String, Bool> = script.flags;
+		trace('flags: ' + flags);
+
+		var options:Map<String, Array<String>> = script.options;
+		trace('options: ' + options);
+
+		var workingDirectory:String = script.workingDirectory;
+		trace('workingDirectory: ' + workingDirectory);
+
+		/*final args:Array<String> = Sys.args();
+			final runDir:Null<String> = args.pop();
+			final command:Null<String> = args.shift();
+			final target:Null<String> = args.shift();
+
+			var platform:Null<TargetPlatform> = null;
+
+			if (runDir != null && command != null && target != null)
 			{
-				case 'build':
-					if (!FileSystem.exists('build.hxml'))
-						Log.error(ANSI.apply('Unable to find "build.hxml" in ' + runDir + ' for the build process.', [ANSICode.Red]));
+				Sys.setCwd(runDir);
 
-					final buildFile:HXML = HXML.fromFile('build.hxml');
-					final configFile:Config = Json.parse(File.getContent('config.json'));
+				switch (command)
+				{
+					case 'build':
+						if (!FileSystem.exists('build.hxml'))
+							Log.error(ANSI.apply('Unable to find "build.hxml" in ' + runDir + ' for the build process.', [ANSICode.Red]));
 
-					platform = getTargetPlatform(target, configFile, buildFile);
+						final buildFile:HXML = HXML.fromFile('build.hxml');
+						final configFile:Config = Json.parse(File.getContent('config.json'));
 
-					if (platform != null)
-					{
-						platform.setup();
+						platform = getTargetPlatform(target, configFile, buildFile);
 
-						final architectures:Array<String> = getArchitectures(args);
+						if (platform != null)
+						{
+							platform.setup();
 
-						if (architectures.length > 0)
-							platform.build(architectures);
-					}
-				default:
-					Log.error(ANSI.apply('Unknown command: ', [ANSICode.Red]) + command);
-			}
-		}
+							final architectures:Array<String> = getArchitectures(args);
+
+							if (architectures.length > 0)
+								platform.build(architectures);
+						}
+					default:
+						Log.error(ANSI.apply('Unknown command: ', [ANSICode.Red]) + command);
+				}
+		}*/
 	}
-
-	private static function getTargetPlatform(target:Null<String>, config:Config, hxml:HXML):Null<TargetPlatform>
-	{
-		var targets:Null<TargetPlatform> = null
-
-		switch (target)
+	/*private static function getTargetPlatform(target:Null<String>, config:Config, hxml:HXML):Null<TargetPlatform>
 		{
-			/*case 'windows': new TargetPlatform.Windows();
-			case 'linux': new TargetPlatform.Linux();
-			case 'macos': new TargetPlatform.MacOS();
-			case 'emscripten': new TargetPlatform.Emscripten();*/
-			case 'android':
-				targetPlatform = new AndroidPlatform(config, hxml);
-			default: 
-				Log.error(ANSI.apply('Unknown target platform: $target', [ANSICode.Red]));
+			var targets:Null<TargetPlatform> = null
 
-				targetPlatform = null;
-		}
-
-		return targetPlatform;
-	}
-
-	private static function getArchitectures(args:Array<String>):Array<Architecture>
-	{
-		final architectures:Array<Architecture> = [];
-
-		for (arg in args)
-		{
-			switch (arg)
+			switch (target)
 			{
-				case '-arm64', '-armv7', '-x86', '-x86_64':
-					final arch:Null<Architecture> = Architecture.fromFlag(arg);
-
-					if (arch != null)
-						architectures.push(arch);
+				case 'windows':
+					targetPlatform = new TargetPlatform.Windows();
+				case 'linux':
+					targetPlatform = new TargetPlatform.Linux();
+				case 'macos':
+					targetPlatform = new TargetPlatform.MacOS();
+				case 'emscripten':
+					targetPlatform = new TargetPlatform.Emscripten();
+				case 'android':
+					targetPlatform = new AndroidPlatform(config, hxml);
 				default:
-					Log.warn(ANSI.apply('Unknown argument: $arg', [ANSICode.Yellow]));
+					Log.error(ANSI.apply('Unknown target platform: $target', [ANSICode.Red]));
+
+					targetPlatform = null;
 			}
+
+			return targetPlatform;
 		}
 
-		return architectures;
-	}
+		private static function getArchitectures(args:Array<String>):Array<Architecture>
+		{
+			final architectures:Array<Architecture> = [];
+
+			for (arg in args)
+			{
+				switch (arg)
+				{
+					case '-arm64', '-armv7', '-x86', '-x86_64':
+						final arch:Null<Architecture> = Architecture.fromFlag(arg);
+
+						if (arch != null)
+							architectures.push(arch);
+					default:
+						Log.warn(ANSI.apply('Unknown argument: $arg', [ANSICode.Yellow]));
+				}
+			}
+
+			return architectures;
+	}*/
 }
