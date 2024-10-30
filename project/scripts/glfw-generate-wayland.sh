0a39ccea4dbefaa9fad9e877cd80ba798eed43ae
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-30 23:01:59 +0200

    Wayland glfw generation script fixes.

--- project/scripts/glfw-generate-wayland.sh
+++ project/scripts/glfw-generate-wayland.sh
@@ -38,55 +38,55 @@ cd wayland-protocols || exit
 
 # Remove old files if they exist
 echo "Removing old Wayland protocols..."
-rm -f "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol.h
-rm -f "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol-code.h
-rm -f "$GLGLFW_PATH"/wayland-fractional-scale-v1-client-protocol.h
-rm -f "$GLGLFW_PATH"/wayland-fractional-scale-v1-client-protocol-code.h
-rm -f "$GLGLFW_PATH"/wayland-xdg-activation-v1-client-protocol.h
-rm -f "$GLGLFW_PATH"/wayland-xdg-activation-v1-client-protocol-code.h
-rm -f "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol.h
-rm -f "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol-code.h
-rm -f "$GLGLFW_PATH"/wayland-viewporter-client-protocol.h
-rm -f "$GLGLFW_PATH"/wayland-viewporter-client-protocol-code.h
-rm -f "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol.h
-rm -f "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol-code.h
-rm -f "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol.h
-rm -f "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol-code.h
-rm -f "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol.h
-rm -f "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/xdg-shell-client-protocol.h
+rm -f "$GLGLFW_PATH"/xdg-shell-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/fractional-scale-v1-client-protocol.h
+rm -f "$GLGLFW_PATH"/fractional-scale-v1-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/xdg-activation-v1-client-protocol.h
+rm -f "$GLGLFW_PATH"/xdg-activation-v1-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/xdg-decoration-client-protocol.h
+rm -f "$GLGLFW_PATH"/xdg-decoration-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/viewporter-client-protocol.h
+rm -f "$GLGLFW_PATH"/viewporter-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/relative-pointer-unstable-v1-client-protocol.h
+rm -f "$GLGLFW_PATH"/relative-pointer-unstable-v1-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/pointer-constraints-unstable-v1-client-protocol.h
+rm -f "$GLGLFW_PATH"/pointer-constraints-unstable-v1-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/idle-inhibit-unstable-v1-client-protocol.h
+rm -f "$GLGLFW_PATH"/idle-inhibit-unstable-v1-client-protocol-code.h
 
 # Generate protocols
 echo "Generating xdg-shell protocol..."
-wayland-scanner private-code ./stable/xdg-shell/xdg-shell.xml "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol-code.h
-wayland-scanner client-header ./stable/xdg-shell/xdg-shell.xml "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol.h
+wayland-scanner private-code ./stable/xdg-shell/xdg-shell.xml "$GLGLFW_PATH"/xdg-shell-client-protocol-code.h
+wayland-scanner client-header ./stable/xdg-shell/xdg-shell.xml "$GLGLFW_PATH"/xdg-shell-client-protocol.h
 
 echo "Generating fractional-scale protocol..."
-wayland-scanner private-code ./staging/fractional-scale/fractional-scale-v1.xml "$GLGLFW_PATH"/wayland-fractional-scale-v1-client-protocol-code.h
-wayland-scanner client-header ./staging/fractional-scale/fractional-scale-v1.xml "$GLGLFW_PATH"/wayland-fractional-scale-v1-client-protocol.h
+wayland-scanner private-code ./staging/fractional-scale/fractional-scale-v1.xml "$GLGLFW_PATH"/fractional-scale-v1-client-protocol-code.h
+wayland-scanner client-header ./staging/fractional-scale/fractional-scale-v1.xml "$GLGLFW_PATH"/fractional-scale-v1-client-protocol.h
 
 echo "Generating xdg-activation protocol..."
-wayland-scanner private-code ./staging/xdg-activation/xdg-activation-v1.xml "$GLGLFW_PATH"/wayland-xdg-activation-v1-client-protocol-code.h
-wayland-scanner client-header ./staging/xdg-activation/xdg-activation-v1.xml "$GLGLFW_PATH"/wayland-xdg-activation-v1-client-protocol.h
+wayland-scanner private-code ./staging/xdg-activation/xdg-activation-v1.xml "$GLGLFW_PATH"/xdg-activation-v1-client-protocol-code.h
+wayland-scanner client-header ./staging/xdg-activation/xdg-activation-v1.xml "$GLGLFW_PATH"/xdg-activation-v1-client-protocol.h
 
 echo "Generating xdg-decoration protocol..."
-wayland-scanner private-code ./unstable/xdg-decoration/xdg-decoration-unstable-v1.xml "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol-code.h
-wayland-scanner client-header ./unstable/xdg-decoration/xdg-decoration-unstable-v1.xml "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol.h
+wayland-scanner private-code ./unstable/xdg-decoration/xdg-decoration-unstable-v1.xml "$GLGLFW_PATH"/xdg-decoration-unstable-v1-client-protocol-code.h
+wayland-scanner client-header ./unstable/xdg-decoration/xdg-decoration-unstable-v1.xml "$GLGLFW_PATH"/xdg-decoration-unstable-v1-client-protocol.h
 
 echo "Generating viewports protocol..."
-wayland-scanner private-code ./stable/viewporter/viewporter.xml "$GLGLFW_PATH"/wayland-viewporter-client-protocol-code.h
-wayland-scanner client-header ./stable/viewporter/viewporter.xml "$GLGLFW_PATH"/wayland-viewporter-client-protocol.h
+wayland-scanner private-code ./stable/viewporter/viewporter.xml "$GLGLFW_PATH"/viewporter-client-protocol-code.h
+wayland-scanner client-header ./stable/viewporter/viewporter.xml "$GLGLFW_PATH"/viewporter-client-protocol.h
 
 echo "Generating relative-pointer protocol..."
-wayland-scanner private-code ./unstable/relative-pointer/relative-pointer-unstable-v1.xml "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol-code.h
-wayland-scanner client-header ./unstable/relative-pointer/relative-pointer-unstable-v1.xml "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol.h
+wayland-scanner private-code ./unstable/relative-pointer/relative-pointer-unstable-v1.xml "$GLGLFW_PATH"/relative-pointer-unstable-v1-client-protocol-code.h
+wayland-scanner client-header ./unstable/relative-pointer/relative-pointer-unstable-v1.xml "$GLGLFW_PATH"/relative-pointer-unstable-v1-client-protocol.h
 
 echo "Generating pointer-constraints protocol..."
-wayland-scanner private-code ./unstable/pointer-constraints/pointer-constraints-unstable-v1.xml "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol-code.h
-wayland-scanner client-header ./unstable/pointer-constraints/pointer-constraints-unstable-v1.xml "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol.h
+wayland-scanner private-code ./unstable/pointer-constraints/pointer-constraints-unstable-v1.xml "$GLGLFW_PATH"/pointer-constraints-unstable-v1-client-protocol-code.h
+wayland-scanner client-header ./unstable/pointer-constraints/pointer-constraints-unstable-v1.xml "$GLGLFW_PATH"/pointer-constraints-unstable-v1-client-protocol.h
 
 echo "Generating idle-inhibit protocol..."
-wayland-scanner private-code ./unstable/idle-inhibit/idle-inhibit-unstable-v1.xml "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol-code.h
-wayland-scanner client-header ./unstable/idle-inhibit/idle-inhibit-unstable-v1.xml "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol.h
+wayland-scanner private-code ./unstable/idle-inhibit/idle-inhibit-unstable-v1.xml "$GLGLFW_PATH"/idle-inhibit-unstable-v1-client-protocol-code.h
+wayland-scanner client-header ./unstable/idle-inhibit/idle-inhibit-unstable-v1.xml "$GLGLFW_PATH"/idle-inhibit-unstable-v1-client-protocol.h
 
 # Go back to original directory
 cd - || exit
